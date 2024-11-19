06da2e1863b76524f5814027c196d32e1590788a
JOSUE GUADALUPE FELIX SANDOVAL <158236251+Josueflx@users.noreply.github.com>
2024-11-19 14:52:38 -0800

    Update 27.s

--- 27.s
+++ 27.s
@@ -1,3 +1,9 @@
+// Instituto Tecnologico de Tijuana
+// Depto de Sistemas y Computacion
+// Ing. Sistemas computacionales
+// Autor: Felix Sandoval Josue Guadalupe
+// Repositorio: 
+
 // Desplazamientos a la izquierda y derecha
 
 /*
