ca67a47179435c26dccbac157897d4b75243b9d7
JOSUE GUADALUPE FELIX SANDOVAL <158236251+Josueflx@users.noreply.github.com>
2024-11-19 14:53:49 -0800

    Update 4.s

--- 4.s
+++ 4.s
@@ -1,3 +1,9 @@
+// Instituto Tecnologico de Tijuana
+// Depto de Sistemas y Computacion
+// Ing. Sistemas computacionales
+// Autor: Felix Sandoval Josue Guadalupe
+// Repositorio: 
+
 /*
 C# equivalent code:
 
