e338bcab735d60a290551cff4f56969a724b314c
JOSUE GUADALUPE FELIX SANDOVAL <158236251+Josueflx@users.noreply.github.com>
2024-11-19 14:51:15 -0800

    Update 22.s

--- 22.s
+++ 22.s
@@ -1,3 +1,9 @@
+// Instituto Tecnologico de Tijuana
+// Depto de Sistemas y Computacion
+// Ing. Sistemas computacionales
+// Autor: Felix Sandoval Josue Guadalupe
+// Repositorio: 
+
 // Conversión de ASCII a entero
 
 /*
